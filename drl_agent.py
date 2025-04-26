44bda6c9b4b73f3b3ee768fbcfaf96232a518739
Giusdagt <peppescooby096@gmail.com>
2025-04-26 18:18:35 +0200

    Update drl_agent.py

--- drl_agent.py
+++ drl_agent.py
@@ -72,6 +72,7 @@ class GymTradingEnv(gym.Env):
         Args:
         mode (str): Modalità di rendering. Default è 'human'.
         """
+        # Il parametro mode non è utilizzato
         return
 
 
