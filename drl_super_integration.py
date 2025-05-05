155564230268b9c5378b1b7978787ef4743734d6
Giusdagt <peppescooby096@gmail.com>
2025-05-05 20:47:46 +0200

    Update drl_super_integration.py

--- drl_super_integration.py
+++ drl_super_integration.py
@@ -20,6 +20,7 @@ import threading
 import time
 import numpy as np
 import joblib
+from drl_agent import DRLSuperAgent
 
 print("drl_super_integration.py caricato ✅")
 
@@ -39,7 +40,6 @@ class DRLSuperManager:
     su array compressi senza occupare risorse.
     """
     def __init__(self, state_size=512):
-        from drl_agent import DRLSuperAgent
         self.super_agents = {
             "PPO": DRLSuperAgent(state_size=state_size),
             "DQN": DRLSuperAgent(state_size=state_size),
