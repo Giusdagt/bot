c2f4c93d2aceee8cf8ada5bb97ed75886e42ce81
Giusdagt <peppescooby096@gmail.com>
2025-05-06 19:40:26 +0200

    Create ai_utils.py

---
+++ ai_utils.py
@@ -0,0 +1,16 @@
+"""
+ai_utils.py
+""""
+from data_handler import (
+    get_normalized_market_data, get_available_assets
+)
+from ai_model import AIModel, fetch_account_balances
+
+def prepare_ai_model():
+    balances = fetch_account_balances()
+    all_assets = get_available_assets()
+    market_data = {
+        symbol: get_normalized_market_data(symbol)
+        for symbol in all_assets
+    }
+    return AIModel(market_data, balances), market_data
